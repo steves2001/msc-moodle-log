0b204422f7642de9731489746bd822ffbeebff9b
steves2001 <me@stephensmith.me.uk>
2015-04-10 10:40:29 +0100

    Added ODS Reading

---
+++ MoodleLogAnalyse/analyse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OdsReadWrite;
+using System.Data;
+
+namespace MoodleLogAnalyse
+{
+    class Analyse
+    {
+        private static OdsReaderWriter fileReader = new OdsReaderWriter();
+        public static DataSet moodleData;
+
+        public static void getData(string filename)
+        {
+           
+            moodleData = fileReader.ReadOdsFile(filename);
+           
+        }
+    }
+}
